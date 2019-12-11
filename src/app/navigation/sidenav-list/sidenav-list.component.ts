99afb2acaac65fcb930bd0b1093cad72f8f509b2
Abhishek <abugautam@gmail.com>
2019-12-11 08:21:08 +0530

    Added auth related classes (service, guard), route protection and setup data for new training exercises

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,19 +1,36 @@
-import { Component, OnInit,EventEmitter,Output } from '@angular/core';
+import { Component, OnInit,EventEmitter,Output, OnDestroy } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
 
   @Output() sidenavClose = new EventEmitter<void>();
-  constructor() { }
+  isAuth : boolean;
+  authSubscription : Subscription;
+
+  constructor(private authService : AuthService) { }
 
   ngOnInit() {
+    this.authSubscription =  this.authService.authChange.subscribe(authStatus =>{
+      this.isAuth = authStatus;
+    });
   }
   onClose(){
     this.sidenavClose.emit();
+  }
+
+  onLogout(){
+    this.onClose();
+    this.authService.logout();
+  }
+
+  ngOnDestroy(){
+    this.authSubscription.unsubscribe();
   }
 
 }
