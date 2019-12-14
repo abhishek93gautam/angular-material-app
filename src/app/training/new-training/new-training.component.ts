15d4e4e64c49d4a96513a56b878b2e5d2b72f4f7
Abhishek <abugautam@gmail.com>
2019-12-14 13:34:06 +0530

    calculating progress based on duration, subjects in training.service rather than event emitters

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, EventEmitter,Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-new-training',
@@ -8,9 +9,6 @@ import { Exercise } from '../exercise.model';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-
-  @Output() trainingStart = new EventEmitter<void>();
-
   exercises : Exercise[] = [];
 
   constructor(private trainingService : TrainingService) { }
@@ -19,8 +17,9 @@ export class NewTrainingComponent implements OnInit {
     this.exercises = this.trainingService.getAvailableExercises();
   }
 
-  onStartTraining(){
-    this.trainingStart.emit();
+  onStartTraining(form : NgForm){
+    console.log(form.value.exercise);
+    this.trainingService.startExercie(form.value.exercise);
   }
 
 }
